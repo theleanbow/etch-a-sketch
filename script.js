391b8714f8db48040b48e4e13ab4b5602975e8fe
LeanBow <theleanbow@gmail.com>
2022-07-08 21:41:50 +0545

    Changed input method to slider

--- script.js
+++ script.js
@@ -2,27 +2,38 @@ let sq = 4;
 const length = 800;
 let div = Array.from(Array(sq), () => new Array(sq));
 container = [];
-const button = document.querySelector("button");
+
+//slider input
+let range = document.querySelector(".range");
+range.addEventListener("click", () => {
+  sq = parseInt(range.value);
+  div = Array.from(Array(sq), () => new Array(sq));
+  clearGrid();
+  createGrid(mainContainer);
+});
 let mainContainer = document.createElement("div");
-button.insertAdjacentElement("afterend", mainContainer);
+range.insertAdjacentElement("afterend", mainContainer);
+
 //take the value of number of rows/columns of the grid from user
-function numberOfSquares() {
-  button.addEventListener("click", function () {
-    do {
-      sq = parseInt(prompt("Enter the number of squares per row "));
-    } while (sq > 100);
-    if (!Number.isNaN(sq)) button.innerText = sq;
-    div = Array.from(Array(sq), () => new Array(sq));
-    clearGrid();
-    createGrid(mainContainer);
-  });
-}
+//changed with input slider
+// const button = document.querySelector("button");
+// function numberOfSquares() {
+//   button.addEventListener("click", function () {
+//     do {
+//       sq = parseInt(prompt("Enter the number of squares per row "));
+//     } while (sq > 100);
+//     if (!Number.isNaN(sq)) button.innerText = sq;
+//     div = Array.from(Array(sq), () => new Array(sq));
+//     clearGrid();
+//     createGrid(mainContainer);
+//   });
+// }
 //remove the existing grid layout before making new one
 function clearGrid() {
   mainContainer.innerHTML = "";
 }
 //creates the grid layout
-function createGrid(button) {
+function createGrid(range) {
   for (let i = 0; i < sq; i++) {
     container[i] = document.createElement("div");
     container[i].style.display = "flex";
@@ -52,5 +63,5 @@ function addTrialColor() {
     });
   }
 }
-createGrid(button);
-numberOfSquares();
+createGrid(range);
+// numberOfSquares();
